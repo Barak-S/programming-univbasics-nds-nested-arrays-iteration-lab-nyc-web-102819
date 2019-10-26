36b9e2186d4269bc97372edb203ff8386647fa89
Barak Saidoff <barak.saidoff@gmail.com>
2019-10-26 16:32:29 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -59,12 +59,14 @@ while row_index < src.count do
     
     if src[row_index][element_index]%2==0
       inner_results << src[row_index][element_index]
-    end
+    
     element_index += 1
+  end
     binding.pry
+    row_index +=1
   end
   outer_results << inner_results
-  row_index += 1
+  # row_index += 1
 end
     inner_results
     
